c9a9614de23bfec8c338af588b3cc117e4a41bc6
ShreyasTantri <shreyastantrik29@gmail.com>
2025-12-19 01:37:14 +0530

    mutating in Structs

--- Structs.playground/Contents.swift
+++ Structs.playground/Contents.swift
@@ -5,10 +5,17 @@ struct Task {
     var statusText: String {
         isDone ? "Done" : "Pending"
     }
+    
+    mutating func markDone() {
+        isDone = true
+    }
 }
 
-var task = Task(title: "Learn Kannada", isDone: true)
+var task = Task(title: "Learn Kannada", isDone: false)
 
 print(task.title)
 print(task.isDone)
 print(task.statusText)
+
+task.markDone()
+print(task.isDone)
